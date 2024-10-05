5a65f50dc6e125ed91308b8e1389f29038868a1e
Dave Silva <github@dmsilva.com>
2024-10-05 15:21:07 -0400

    Add temp single-player button to bypass lobby

--- game/contexts/title_context/title_context.gd
+++ game/contexts/title_context/title_context.gd
@@ -4,6 +4,7 @@ class_name TitleContext
 const CONTEXT_ID = "context.title"
 
 onready var start_button = $start_button
+onready var single_player_button = $single_player_button
 
 func context_id_string() -> String:
 	return CONTEXT_ID
@@ -11,12 +12,17 @@ func context_id_string() -> String:
 
 func _ready():
 	start_button.connect("pressed", self, "_on_start_clicked")
+	single_player_button.connect("pressed", self, "_on_single_player_clicked")
 	
 	
 func _on_start_clicked():
 	_on_go_to_game_pressed()
-	
-	
+
+
+func _on_single_player_clicked():
+	change_context(GameplayContext.CONTEXT_ID)
+
+
 func _on_go_to_game_pressed():
 	print('hello')
 	change_context(LobbyContext.CONTEXT_ID)
