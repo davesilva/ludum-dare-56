0764e721cc3383aff7b532d8dc29bcfb8039bd6e
Baja The Frog <bajathefrog@gmail.com>
2024-10-07 10:51:04 -0400

    Pads rotate!

--- game/items/speed_pad.gd
+++ game/items/speed_pad.gd
@@ -5,3 +5,12 @@ func _ready():
 	add_to_group(Game.groups.types.environment)
 	add_to_group(Game.groups.roots.player_character)
 	$hotbox.add_to_group(Game.groups.hotboxes.speed_pad)
+
+
+func _on_rotation_timer_timeout():
+	rotation_degrees += 90
+	rotation_degrees = fmod(rotation_degrees, 360.0)
+	$arrows.rotation_degrees = -90
+	var tween = create_tween()
+	tween.tween_property($arrows, "rotation_degrees", 0, 0.3)
+	
