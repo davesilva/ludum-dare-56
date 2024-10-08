65e810c1ef0fbebe1c5c1deeeb67ea4210d00b81
Dave Silva <github@dmsilva.com>
2024-10-08 17:54:59 -0400

    Add a github actions job to build the docker image

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build --platform linux/x86_64 -t davesilva/ludum-dare-56:$(git rev-parse --short HEAD) .
-docker push davesilva/ludum-dare-56:$(git rev-parse --short HEAD)
+docker build --platform linux/x86_64 -t davesilva/ludum-dare-56:$(git rev-parse HEAD) .
+docker push davesilva/ludum-dare-56:$(git rev-parse HEAD)
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 source $(dirname "$0")/build-server.sh
 source $(dirname "$0")/build-web.sh
-ssh root@ld56.dmsilva.com -t "echo $(git rev-parse --short HEAD) > /root/CURRENT_REV_BETA && ./docker-restart-beta.sh"
+ssh root@ld56.dmsilva.com -t "echo $(git rev-parse HEAD) > /root/CURRENT_REV_BETA && ./docker-restart-beta.sh"
 scp -r $(dirname "$0")/../build/web/* root@137.184.55.161:/var/www/html/beta
